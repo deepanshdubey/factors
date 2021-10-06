6c7e4b9199632ab16b87d99fe9e170e88d9783bb
Ashish Singla <56113524+Ashish0012@users.noreply.github.com>
2021-10-06 12:19:36 +0530

    Update prime_factor.java

--- prime_factor.java
+++ prime_factor.java
@@ -5,38 +5,42 @@
 
 import java.io.*;
 class prime_factor
-{  
+{	
 	public static void main(String args[])throws IOException
-	{
-		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+	{	
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
 		long num, i;
-		String cont, factor="";
+		String cont, factor = "";
 
 		while(true)
 		{    
-			System.out.println("\nInput a number or 0 to exit:");
-			num=Long.parseLong(br.readLine());
-			if(num==1)
+			System.out.println("\nInput a number or 0 to exit : ");
+			num = Long.parseLong(br.readLine());
+			
+			if(num == 1)
 			{
 				System.out.println("1 = 1");
 				continue;
 			}
-			else if(num==0)
+			else if(num == 0)
 			{
 				break;
 			}
 			
 			// Factors of numbers greater than 1
-			System.out.print(num+" = ");
-			factor="";
-			for(i=2; i<=num; i++)
+			System.out.print(num + " = ");
+			factor = "";
+			
+			for(i = 2; i <= num; i++)
 			{
-				while(num%i==0)
+				while(num % i == 0)
 				{
 					factor += " x " + Long.toString(i);
 					num = num / i;
 				}
 			}
+			
 			System.out.println(factor.substring(3));
 		}
 	}
